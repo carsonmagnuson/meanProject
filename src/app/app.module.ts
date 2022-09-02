f05e74ce4fdb20036ca97e8d31b43d3daa17a362
carsonmagnuson <carsonmagnuson@gmail.com>
2022-09-02 10:57:54 -0400

    added functionality to posts system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,28 @@ import {FormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {HeaderComponent} from "./header/header.component";
+import {PostListComponent} from "./posts/post-list/post-list.component";
+import {MatExpansionModule} from "@angular/material/expansion";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
