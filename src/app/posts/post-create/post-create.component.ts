d8a114f33135b8fb451795a3b10637f7ad0744b7
carsonmagnuson <carsonmagnuson@gmail.com>
2022-09-03 11:27:37 -0400

    added basic backend start

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,22 +1,23 @@
-import {Component, EventEmitter, Output} from "@angular/core";
-import {Post} from "../post.model";
+import {Component} from "@angular/core";
+import {NgForm} from "@angular/forms";
+import {PostsService} from "../posts.service";
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent{
-  enteredContent = '';
-  enteredTitle = '';
-  @Output() postCreated = new EventEmitter<Post>();
+export class PostCreateComponent {
 
-  onAddPost() {
-    const post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+  constructor(public postsService: PostsService) {
+  }
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
     }
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content)
+    form.resetForm();
   }
 
 }
