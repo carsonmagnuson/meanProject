42036e712fcc570cb6a4df58e41508b459b09683
carsonmagnuson <carsonmagnuson@gmail.com>
2022-09-07 21:04:37 -0400

    added basic backend routing

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const Post = require('./models/post');
+
+const postsRoutes = require('./routes/posts')
 
 const app = express();
 const api_key = process.env.API_KEY;
@@ -32,57 +33,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts', (req, res, next) => { // change our posts
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then((createdPost) => {
-    res.status(201).json({ // 201 means 'all g, new resource created'
-      message: 'Posts added successfully.',
-      postId: createdPost._id
-    });
-  });
-  console.log(post);
-});
-
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  })
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    console.log(result);
-    res.status(200).json({message: "Update successful."});
-  });
-});
-
-app.get('/api/posts', (req, res, next) => { // get our posts
-  Post.find()
-    .then(documents => {
-      res.status(200).json({ //200 means 'all g'
-        message: 'Posts fetched successfully.',
-        posts: documents
-      });
-    });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({message: 'Post not found.'})
-    }
-  });
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({_id: req.params.id}).then((result) => {
-    console.log(result);
-    res.status(200).json({message: "Post deleted."});
-  });
-});
+app.use('/api/posts', postsRoutes);
 
 module.exports = app;
