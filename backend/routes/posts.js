02cdd3d60536b5a8c12239735c5f6606bfad82c7
carsonmagnuson <carsonmagnuson@gmail.com>
2022-09-09 11:54:45 -0400

    added image changing functionality

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post('', multer({storage: storage}).single("image"),(req, res, next) => { // change our posts
+router.post('', multer({storage: storage}).single("image"), (req, res, next) => { // change our posts
   const url = req.protocol + '://' + req.get('host');
   const post = new Post({
     title: req.body.title,
@@ -45,17 +45,27 @@ router.post('', multer({storage: storage}).single("image"),(req, res, next) => {
   console.log(post);
 });
 
-router.put('/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  })
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    console.log(result);
-    res.status(200).json({message: "Update successful."});
+router.put(
+  '/:id',
+  multer({storage: storage}).single("image"),
+  (req, res, next) => {
+    let imagePath = req.body.imagePath;
+    if (req.file) {
+      const url = req.protocol + '://' + req.get('host');
+      imagePath = url + "/images/" + req.file.filename;
+    }
+    const post = new Post({
+      _id: req.body.id,
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: imagePath,
+    })
+    console.log(req.file);
+    Post.updateOne({_id: req.params.id}, post).then(result => {
+      console.log(result);
+      res.status(200).json({message: "Update successful."});
+    });
   });
-});
 
 router.get('', (req, res, next) => { // get our posts
   Post.find()
